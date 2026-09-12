4e3d7355b30977b6c87de404c64392454fb35427
mcode-bot@modelcode.ai <mcode-bot@modelcode.ai>
2026-09-12 18:41:41 +0000

    Fix review issue #4: Move diskpart script creation inside ShouldProcess to prevent temp file leak in WhatIf mode

--- checkandfix/src/Export-Installation.psm1
+++ checkandfix/src/Export-Installation.psm1
@@ -105,9 +105,8 @@ function Export-ToUSB {
     # ------------------------------------------------------------------
     Write-StepResult -Message 'Step 2/4: Preparing USB drive via diskpart' -Status 'Info'
 
-    $scriptPath = New-DiskpartScript -DiskNumber $DiskNumber -DriveLetter $DriveLetter
-
     if ($PSCmdlet.ShouldProcess("Disk $DiskNumber", 'Run diskpart to prepare USB drive')) {
+        $scriptPath = New-DiskpartScript -DiskNumber $DiskNumber -DriveLetter $DriveLetter
         $diskpartResult = Invoke-Diskpart -ScriptPath $scriptPath
     }
     else {
